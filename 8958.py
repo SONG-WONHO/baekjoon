286504c110394a1ea9af4851ecb911ee6bd7211e
user-PC\user <win778500@gmail.com>
2017-07-28 14:20:27 +0900

    8958 - run_time_error

---
+++ 8958.py
@@ -0,0 +1,24 @@
+num = input()
+in_ox = []
+
+for i in range(int(num)):
+    in_ox.append(input())
+
+
+for _, string in enumerate(in_ox):
+    string = list(string)
+
+    logits = []
+    total_num = 0
+
+    for _, c in enumerate(string):
+        if c == 'O':
+            logits.append(True)
+
+            for _, c in enumerate(logits):
+                total_num += 1
+
+        else:
+            logits = []
+
+    print(total_num)
