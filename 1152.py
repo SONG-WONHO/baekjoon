baf89974d8c58e70f44c7cb089ccc9e7cefbd37d
Win7-1405020009\ml <win778500@gmail.com>
2017-07-31 22:42:25 +0900

    1152(2)

--- 1152.py
+++ 1152.py
@@ -1,10 +1,3 @@
-in_str = input()
-in_str = list(in_str)
+in_str = input().split()
 
-num = 1
-
-for i in range(len(in_str)):
-    if in_str[i] == ' ':
-        num += 1
-
-print(num)
+print(len(in_str))
