9c1d8a859bb2231baa886b3edfb1bcc1e908d792
Win7-1405020009\ml <win778500@gmail.com>
2017-07-31 22:42:51 +0900

    2675(2)

--- 2675.py
+++ 2675.py
@@ -1,15 +1,15 @@
-num = input()
+num = int(input())
 
 str_list = []
 
-for _ in range(int(num)):
-    input_str = input()
+for _ in range(num):
+    input_str = str(input())
 
     str_list.append(input_str)
 
-for c in str_list:
-    pasre_num = c[0]
-    parse_str = c[2:]
+for string in str_list:
+    pasre_num = string[0]
+    parse_str = string[2:]
 
     result = ""
     for k in parse_str:
